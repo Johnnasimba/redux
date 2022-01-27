3fe287d70adee9dd489778bb45835dca9eebda54
John <nasimba4john@gmail.com>
2022-01-27 08:40:28 +0200

    reducers and actions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const userReducer = ( state = {id: 1}, action) => {
+  switch(action.type){
+    case "USER_NAME": {
+      state = { ...state, name: action.payload }
+      break
+    } 
+    case "USER_AGE": {
+      state = { ...state, age: action.payload }
+      break
+    }
+  }
+  return state;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const jobProfileReducer = (state = {}, action) => {
+  switch(action.type){
+    case "JOB_DETAIL": {
+      state = { ...state, job: action.payload }
+      break
+    } 
+  }
+  return state;
+
+}
+const reducers = combineReducers({
+  user : userReducer,
+  jobProfile : jobProfileReducer
+})
+
+const store = createStore( reducers );
+
+store.subscribe( () => {
+  console.warn('My store has changed ', store.getState());
+});
+
+function getUsername(){
+  return {
+    type: 'USER_NAME',
+    payload: 'John'
+  }
+}
+function getUserAge(){
+  return {
+    type: 'USER_AGE',
+    payload: 24
+  }
+}
+
+function getJobDetail () {
+  return {
+    type: 'JOB_DETAIL',
+    payload: 'Product developer'
+  }
+}
+
+store.dispatch(getUsername())
+store.dispatch(getUserAge())
+store.dispatch(getJobDetail())
